48a38603c3092e67f14dd0cccb803fee0acf9064
“John <john@email.com>
2026-04-06 19:42:41 +0300

    +numbers list +grades list

---
+++ methods_of_lists.py
@@ -0,0 +1,24 @@
+numbers = [1, 3, 4]
+names = ['Anna', "Ivan"]
+mixed = [
+    1,
+    90.901,
+    True,
+    False,
+    "some text",
+
+    ['123', 1232],
+
+]
+
+first_element = mixed[0]
+print(first_element)
+
+third_element = mixed[2]
+print(third_element)
+
+last_element = mixed[-1]
+print(last_element)
+
+last_element_in_last_element = last_element[-1]
+print(last_element_in_last_element)
