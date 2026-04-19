a3b43b2bd1da07761ad090b897a293b7b6fb74d1
“John <john@email.com>
2026-04-19 14:48:56 +0300

    + dz_10

--- dicts_web_practice_products.py
+++ dicts_web_practice_products.py
@@ -1,11 +1,11 @@
 import requests
 from pprint import pprint
+
+url_products = 'https://dummyjson.com/products'
 params = {
     "skip": 0,
     "limit": 1000,
 }
-
-url_products = 'https://dummyjson.com/products'
 response = requests.get(url=url_products, params=params)
 
 response_json = response.json()
@@ -15,7 +15,9 @@ products = response_json['products']
 total_cost = 0
 for product in products:
     product_cost = product['price'] * product['stock']
-    total_cost = total_cost + product_cost
+    # total_cost = total_cost + product_cost
+    total_cost += product_cost
+    print(total_cost)
 
 total_cost = round(total_cost, 2)
-print(f'{total_cost = }')
+print(f'{total_cost=}')
