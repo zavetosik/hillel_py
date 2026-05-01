d37e83e7720254b7e4e217d2f55b1346ccffc3be
“John <john@email.com>
2026-05-01 08:52:17 +0300

    fix dz

--- functions_repeat.py
+++ functions_repeat.py
@@ -9,17 +9,17 @@ def get_average(number_1: int | float, number_2: int | float, number_3: int | fl
 
 
 def get_check(number_check: int | float) -> bool:
-    foo_result = number_check % 2 == 0 and number_check > 10
-    return foo_result
+    check_result = number_check % 2 == 0 and number_check > 10
+    return check_result
 
 def count_vowels(text_vowels: str) -> int | float:
-    count = 0
+    count_result = 0
 
     for char in text_vowels:
         if char in vowels:
-            count += 1
+            count_result += 1
 
-    return count
+    return count_result
 
 
 
